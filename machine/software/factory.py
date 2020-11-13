e79a1f8c448aa091a43018f962473134c495e741
Will <willcruse123@gmail.com>
2020-11-13 22:13:46 +0000

    feat(machine/software): :sparkles: Create a discoverer to find pumps and pump factory to create pumps

---
+++ machine/software/factory.py
@@ -0,0 +1,20 @@
+import inspect
+import pumps
+
+class Discoverer:
+    def __init__(self):
+        self.objects= {}
+        for name, obj in inspect.getmembers(pumps):
+            if inspect.isclass(obj):
+                self.objects[name] = obj
+
+class Factory:
+    def __init__(self):
+        self._discoverer = Discoverer()
+
+    def pump_factory(self, pump_type, pump_config):
+        try:
+            return self._discoverer.objects[pump_type](pump_config['pins'], pump_config['switch_pin'], pump_config['contents'])
+        except KeyError as exe:
+            print(self._discoverer.objects)
+            raise Exception("Invalid Pump Type") from exe
